54a655c58c1f451ed98bd43208091b6cacc45914
zzq <3074127187@qq.com>
2019-01-03 20:43:16 +0800

    搭建脚手架

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="footer">
+		<div class="footer-wrap ft-fixed">
+		   	<ul>
+		   	 	<li >	
+		   	 		<i class="iconfont ui-nav1"></i> 
+		   	 		<p class="tab-text">首页</p>
+		   	 	</li>
+		    	<li >	
+		    		<i class="iconfont ui-nav3"></i> 
+		    		<p class="tab-text">分类</p>
+		    	</li>
+			    <li >	
+			    	<i class="iconfont ui-nav4"></i> 
+			    	<p class="tab-text">购物车</p>
+			    </li>
+			    <li >	
+			    	<i class="iconfont ui-nav5"></i> 
+			    	<p class="tab-text">我</p>
+			    </li>
+		   	</ul>
+  		</div>
+	</div>
+</template>
+<script type="text/javascript">
+	
+</script>
+<style type="text/css" lang='scss' scoped>
+	
+</style>
