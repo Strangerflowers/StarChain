a0eb0971301010718c137651726ffd9e9e6eb114
zzq <3074127187@qq.com>
2019-01-21 15:05:20 +0800

    路由拦截

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import Goods from '../page/Goodslist/goodslist.vue';
 import Detail from '../page/Detail/detail.vue';
 import Slist  from '../components/searList.vue';
 import Good  from '../components/goodsearch.vue';
+import Login  from '../page/Login/login.vue';
+import Register  from '../Register/register.vue';
 
 
 
@@ -18,14 +20,62 @@ import Good  from '../components/goodsearch.vue';
 const routes = [
 	// {path:'/',redirect:{name:'Home'}}
 	{path:'/',redirect:{name:'Home'}},  //重定向
-	{name:'Home',path:'/home',component:Home},
-	{name:'Classify',path:'/classify',component:Classify},
-	{name:'Cart',path:'/cart',component:Cart},
-	{name:'User',path:'/user',component:User},
-	{name:'Goods',path:'/goodslist',component:Goods},
-	{name:'Detail',path:'/detail',component:Detail},
-	{name:'Slist',path:'/searchlist',component:Slist},
-	{name:'Good',path:'/goodsearch/:keyword',component:Good}
+	{
+		name:'Home',
+		path:'/home',
+		component:Home
+	},
+	{
+		name:'Classify',
+		path:'/classify',
+		component:Classify
+	},
+	{
+		name:'Cart',
+		path:'/cart',
+		component:Cart,
+		meta:{
+			requireAuth:true
+		}
+	},
+	{
+		name:'User',
+		path:'/user',
+		component:User,
+		meta:{
+			requireAuth:true
+		}
+	},
+	{
+		name:'Goods',
+		path:'/goodslist',
+		component:Goods
+	},
+	{
+		name:'Detail',
+		path:'/detail',
+		component:Detail
+	},
+	{
+		name:'Slist',
+		path:'/searchlist',
+		component:Slist
+	},
+	{
+		name:'Good',
+		path:'/goodsearch/:keyword',
+		component:Good
+	},
+	{
+		name:'Login',
+		path:'/login/',
+		component:Login
+	},
+	{
+		name:'Register',
+		path:'/register/',
+		component:Register
+	}
 	
 ]
 
@@ -33,7 +83,26 @@ const routes = [
 let router = new VueRouter({
 	 // mode:'history', //加了这句能把url地址栏的#去掉
 	routes
-})
+});
+// 全局路由守卫
+router.beforeEach((to,from,next)=>{
+	if(to.meta.requireAuth){
+		if(sessionStorage.getItem('token')){
+			next();
+		}else{
+			next({
+				path:'./login',
+				query: {redirect: to.fullPath} // 登陆成功后回到当前页面，这里传值给login页面，to.fullPath为当前点击的页面
+
+			})
+		}
+	}else{
+		next();
+	}
+});
+// router.afterEach((to,from)=>{
+// 	console.log('after')
+// })
 
 export default router;
 
